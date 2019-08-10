17e83f3ff7eae5cf0c7e2b45c4f8dcf640546b87
jp-hong <jphong0603@gmail.com>
2019-08-11 01:42:48 +0900

    minor changes

--- classifier/code/preprocessor.py
+++ classifier/code/preprocessor.py
@@ -6,19 +6,6 @@ from urllib.parse import urlparse, quote
 import numpy as np
 
 
-def clean(url):
-
-    """
-    Return url after cleaning. Will remove 'http://', 'https://', 'www.',
-    and any trailing '/'. Will also percent encode any non-ascii
-    characters
-    """
-
-    p = "(https|http)?(://)?(www2?\.)?"
-    temp = re.sub(p, "", url).rstrip("\/")
-    return quote(temp, safe=string.printable)
-
-
 def tokenize(url, include_separators=False):
 
     """
@@ -33,6 +20,7 @@ def tokenize(url, include_separators=False):
 
     return list(filter(None, re.split(r"[" + seps + r"]", url)))
 
+
 def char_level_encoder(s):
     c_dict = {string.printable[i]:i for i in range(len(string.printable))}
     vect = np.array([0] * 128)
@@ -41,7 +29,4 @@ def char_level_encoder(s):
         vect[i] = c_dict[s[i]]
         
     return vect
-
-def word_level_encoder(s):
-    pass
 
--- classifier/code/util.py
+++ classifier/code/util.py
@@ -2,10 +2,12 @@
 
 import pickle
 
+
 def save(obj, path):
     with open(path, "wb") as f:
         pickle.dump(obj, f)
 
+        
 def load(path):
     with open(path, "rb") as f:
         obj = pickle.load(f)
