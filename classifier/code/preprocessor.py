86084aefaaa8734040d9c84b26a09cc316d2048a
jp-hong <jphong0603@gmail.com>
2019-08-12 15:11:18 +0900

    padding is optional

--- classifier/code/preprocessor.py
+++ classifier/code/preprocessor.py
@@ -20,13 +20,13 @@ def tokenize(url, include_separators=False):
     return list(filter(None, re.split("[" + seps + "]", url)))
 
 
-def char_level_encoder(url, ndim=128):
+def char_level_encoder(url, ndim=128, pad=True):
     if len(url) > ndim:
         url = url[:ndim]
     
     vect = list(map(ord, list(url)))
 
-    if len(vect) < ndim:
+    if pad and len(vect) < ndim:
         vect += [0] * (ndim - len(vect))
 
     return np.array(vect)
