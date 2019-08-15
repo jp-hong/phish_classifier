0decc892e2d3c34021864ef9fe2f0eb1f9ef2ed5
jp-hong <jphong0603@gmail.com>
2019-08-16 00:34:23 +0900

    add timing to workflow()

--- classifier/code/modelhelper.py
+++ classifier/code/modelhelper.py
@@ -1,5 +1,6 @@
 
 
+from time import perf_counter
 from os import cpu_count
 from sklearn.metrics import classification_report
 from sklearn.model_selection import cross_val_score
@@ -9,15 +10,22 @@ from .util import print_cv_score
 
 def workflow(model, label, x_train, x_test, y_train, y_test, n_jobs):
 
+    start = perf_counter()
     fit_result = model.fit(x_train, y_train)
     print(fit_result)
     print()
+    end = perf_counter()
+    fit_time = end - start
 
+    start = perf_counter()
     y_pred = model.predict(x_test)
 
     print("MODEL: {}".format(label))
     print(classification_report(y_test, y_pred))
+    end = perf_counter()
+    pred_time = end - start
 
+    start = perf_counter()
     cv_scores = cross_val_score(
         estimator=model,
         X=x_train,
@@ -28,5 +36,11 @@ def workflow(model, label, x_train, x_test, y_train, y_test, n_jobs):
     )
 
     print_cv_score(cv_scores, label=label)
+    end = perf_counter()
+    cv_time = end - start
+
+    print("\nElapsed time: %0.2fs" % (fit_time + pred_time + cv_time), end=" ")
+    print("[fit %0.2fs, predict %0.2fs, cv: %0.2fs]" % \
+        (fit_time, pred_time, cv_time))
 
     return cv_scores
