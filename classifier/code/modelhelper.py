74e31544f6f0d50521f91be9bd9a4bb5a4dafe69
jp-hong <jphong@outlook.com>
2019-08-18 23:24:34 +0900

    print K in results for K-fold cv

--- classifier/code/modelhelper.py
+++ classifier/code/modelhelper.py
@@ -6,13 +6,13 @@ from sklearn.metrics import classification_report
 from sklearn.model_selection import cross_val_score, cross_validate
 
 
-def print_cv_score(scores, label=None, metric=None):
+def print_cv_score(scores, K, label=None, metric=None):
     if label:
-        print("Cross validation score: %0.4f (+/- %0.4f) [%s / %s]" % \
-            (scores.mean(), scores.std(), metric, label))
+        print("Cross validation score: %0.4f (+/- %0.4f) [K=%d / %s / %s]" % \
+            (K, scores.mean(), scores.std(), metric, label))
     else:
-        print("Cross validation score: %0.4f (+/- %0.4f) [%s]" % \
-            (scores.mean(), scores.std(), metric))
+        print("Cross validation score: %0.4f (+/- %0.4f) [K=%d / %s]" % \
+            (K, scores.mean(), scores.std(), metric))
 
 
 def print_run_time(f_time, p_time, cv_time):
@@ -21,7 +21,7 @@ def print_run_time(f_time, p_time, cv_time):
         (f_time, p_time, cv_time))
 
 
-def workflow(model, label, scoring, n_cv, x_train, x_test, 
+def workflow(model, label, scoring, K, x_train, x_test, 
     y_train, y_test, n_jobs=4):
 
     start = perf_counter()
@@ -46,12 +46,12 @@ def workflow(model, label, scoring, n_cv, x_train, x_test,
         X=x_train,
         y=y_train,
         scoring=scoring,
-        cv=n_cv,
+        cv=K,
         n_jobs=n_jobs
     )
 
     for key in cv_scores.keys():
-        print_cv_score(cv_scores[key], label=label, metric=key)
+        print_cv_score(cv_scores[key], K, label=label, metric=key)
 
     end = perf_counter()
     cv_time = end - start
